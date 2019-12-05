994929f18968cc0dce630fe1181fb6bd8ed53a4a
Petar Trifonov Petrov <petarpetrovt@gmail.com>
2019-12-05 21:31:09 +0200

    Improve deploy script

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,5 +1,6 @@
 $scriptPath = Split-Path -Parent $PSCommandPath
 $deployPath = Join-Path -Path $scriptPath -ChildPath ".deploy"
+$packagesDeployPath = Join-Path -Path $deployPath -ChildPath "packages"
 
 Write-Host "Clearing deploy folder: $deployPath"
 Remove-Item $deployPath -Recurse -Force
@@ -11,4 +12,6 @@ Write-Host "Testing PlatformSpecific XUnit"
 dotnet test test/PlatformSpecific.XUnit.Tests/PlatformSpecific.XUnit.Tests.csproj -c Release
 
 Write-Host "Deploying packages"
-dotnet pack -o $deployPath
+dotnet pack -o $packagesDeployPath
+
+Write-Host "Done"
